780cd550e153960cdef1be8dc61b2a1df9a114d1
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-04-17 16:16:11 +0900

    Add benchmark (criterion)

---
+++ benches/cic_bench.rs
@@ -0,0 +1,19 @@
+use cic_fixed::CicDecimationFilter;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn cic_decimation_benchmark(c: &mut Criterion) {
+    let mut filter = CicDecimationFilter::<64, 5>::new();
+    c.bench_function("cic", |b| {
+        b.iter(|| {
+            if let Some(_x) = filter.filter(black_box(1)) {
+                // do nothing
+            }
+            if let Some(_x) = filter.filter(black_box(-1)) {
+                // do nothing
+            }
+        })
+    });
+}
+
+criterion_group!(benches, cic_decimation_benchmark);
+criterion_main!(benches);
