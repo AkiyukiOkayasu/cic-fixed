309e1e292e0cc9f83acb8239bc3c7c026bfeeb37
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-09-20 16:06:10 +0900

    Rename filter() to process_sample()

--- benches/cic_bench.rs
+++ benches/cic_bench.rs
@@ -14,10 +14,10 @@ fn cic_decimation_benchmark(c: &mut Criterion) {
     let mut filter = CicDecimationFilter::<64, 5>::new();
     c.bench_function("cic", |b| {
         b.iter(|| {
-            if let Some(_x) = filter.filter(black_box(&1)) {
+            if let Some(_x) = filter.process_sample(black_box(&1)) {
                 // do nothing
             }
-            if let Some(_x) = filter.filter(black_box(&-1)) {
+            if let Some(_x) = filter.process_sample(black_box(&-1)) {
                 // do nothing
             }
         })
